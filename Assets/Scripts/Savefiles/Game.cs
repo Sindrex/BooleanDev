2a5903d4bf9ca37e2f956f4737dfeea832b7bd67
Sindre Haugland Paulshus <Sindre2009@live.no>
2026-09-23 20:18:56 +0200

    1.2.0 Added Notgate curved and OneWayer curved

--- Assets/Scripts/Savefiles/Game.cs
+++ Assets/Scripts/Savefiles/Game.cs
@@ -30,7 +30,7 @@ public class Game {
     //History
     public string dateCreated;
     public string dateLastPlayed;
-    public string version = "1.1.1";
+    public string version = "1.2.0";
 
     public Game(EOTP_PuzzleCreator puzzle)
     {
